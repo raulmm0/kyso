f579017c3eb30ccead0a912ba2e0e8b0238ce61f
Raul <45597254+raulmm0@users.noreply.github.com>
2023-09-14 10:04:50 +0200

    v0.1
    Comments and error control have been added, and the key pressed has also been changed to avoid problems.
    
    Se han añadido comentarios y control de errores, además se cambio la tecla presionada para evitar problemas.

--- dontSleep.bat
+++ dontSleep.bat
@@ -1,10 +1,29 @@
 @if (@CodeSection == @Batch) @then
 @echo off
 set sendKey=CScript //nologo //E:JScript "%~F0"
-:a
-%sendKey% {F15}
+
+:LOOP
+REM Envía la tecla F14 para evitar el bloqueo de pantalla
+%sendKey% {F14}
+
+REM Espera 290 segundos antes de volver a enviar la tecla
 timeout 290 > nul
-goto a
+
+REM Comprueba si ocurrió un error durante el envío de tecla
+IF ERRORLEVEL 1 (
+    @echo on
+    echo Error al enviar la tecla. Saliendo.
+    exit /b 1
+)
+
+goto LOOP
 @end
+
 var WshShell = WScript.CreateObject("WScript.Shell");
-WshShell.SendKeys(WScript.Arguments(0));
+try {
+    // Obtiene el argumento que contiene la tecla a enviar y envía la tecla especificada
+    WshShell.SendKeys(WScript.Arguments(0));
+} catch (e) {
+    WScript.Echo("Error: " + e.message);
+    WScript.Quit(1);
+}
